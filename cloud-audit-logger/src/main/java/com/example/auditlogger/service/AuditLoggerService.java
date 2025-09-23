3a0c83f118aa0e49f4d927630034df8a590caf05
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-09-23 17:36:24 +0530

    refactor

--- cloud-audit-logger/src/main/java/com/example/auditlogger/service/AuditLoggerService.java
+++ cloud-audit-logger/src/main/java/com/example/auditlogger/service/AuditLoggerService.java
@@ -1,18 +1,32 @@
 package com.example.auditlogger.service;
 
+import javax.annotation.PostConstruct;
+
 public class AuditLoggerService {
 
     private final Authenticator authenticator;
 
     private final AuditEventLogger eventLogger;
 
+    private boolean loggingEnabled = false;
+
     public AuditLoggerService(Authenticator authenticator, AuditEventLogger eventLogger) {
         this.authenticator = authenticator;
         this.eventLogger = eventLogger;
     }
 
+    @PostConstruct
+    public void init() {
+        if (authenticator.authenticate()) {
+            loggingEnabled = true;
+            eventLogger.log("Audit Logger Initialized", authenticator.getUsername());
+        } else {
+            throw new RuntimeException("Audit Logger could not authenticate!");
+        }
+    }
+
     public void logEvent(String event) {
-        if(authenticator.authenticate()) {
+        if (loggingEnabled) {
             eventLogger.log(event, authenticator.getUsername());
         } else {
             System.out.println("Unauthorized attempt to log event: " + event);
